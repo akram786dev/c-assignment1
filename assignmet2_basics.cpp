6b17b91105c2ba6602ca24ff5c1d9ef5454569bd
mdakramhussain0000-cpu <mdakramhussain0000@gmail.com>
2026-03-23 15:47:24 +0530

    first commit

---
+++ assignmet2_basics.cpp
@@ -0,0 +1,86 @@
+// // 
+// // 
+// /*write a c++ program to calculate the volume of a cuboid*/
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//    float length, breadth, height, volume;
+//    cout<<"enter length breadth and height:";
+//    cin>>length>>breadth>>height;
+//    volume = length*breadth*height;
+//    cout<<"volume of cuboid is:"<<volume;
+//     return 0;
+// }// // // // //write a c++ program to print Hello My SirG on the screen
+// // // // // // #include<iostream>
+// // // // // // using namespace std;
+// // // // // // int main()
+// // // // // // {
+// // // // // //     cout<<"Hello My SirG";
+// // // // // //     cout<<endl;
+// // // // // //     return 0;
+// // // // // // }
+// // // // // /*write a c++ program to print hello on the screen on the first 
+// // // // // line and My SirG on the second line using endl*/
+// // // // // #include<iostream>
+// // // // // using namespace std;
+// // // // // int main()
+// // // // // {
+// // // // //     cout<<"Hello"<<endl<<"MySirG";
+// // // // //     return 0;
+// // // // // }
+// // // // /*write a c++ program to calculate the sum of two numbers*/
+// // // // #include<iostream>
+// // // // using namespace std;
+// // // // int main()
+// // // // {
+// // // //     int a , b;
+// // // //     cout<<"enter two numbers:\n";
+// // // //     cout<<"enter a:";
+// // // //     cin>>a;
+// // // //     cout<<"enter b:";
+// // // //     cin>>b;
+// // // //     cout<<"sum of "<<a<<" and "<<b<<" is: "<<a+b;
+// // // //     return 0;
+// // // // }
+
+// // // /*write a to clculate the area of circle*/
+// // // #include<iostream>
+// // // using namespace std;
+// // // int main()
+// // // {
+// // //     // area of circle formula is:pi*r*r;
+// // //     const float pi = 3.14;
+// // //     float radius;
+// // //     cout<<"enter radius:";
+// // //     cin>>radius;
+// // //     cout<<"area of circle is:"<<pi*radius*radius;
+// // //     return 0;
+// // // }
+
+
+// // /*write a c++ program to calculate the volume*/
+// // #include<iostream>
+// // using namespace std;
+// // int main()
+// // {
+// //    float side, volume;
+// //    cout<<"enter sides:";
+// //    cin>>side;
+// //    cout<<"volume is:"<<side*side*side;
+// //     return 0;
+// // }
+
+
+// /*write a c++ program to calculate the volume of a cuboid*/
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//    float length, breadth, height, volume;
+//    cout<<"enter length breadth and height:";
+//    cin>>length>>breadth>>height;
+//    volume = length*breadth*height;
+//    cout<<"volume of cuboid is:"<<volume;
+//     return 0;
+// }
