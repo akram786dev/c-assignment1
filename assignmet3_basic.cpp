839b5babdf285c653875196cde723a7de7eb9b0e
mdakramhussain0000-cpu <mdakramhussain0000@gmail.com>
2026-03-23 15:53:20 +0530

    first cheak

---
+++ assignmet3_basic.cpp
@@ -0,0 +1,67 @@
+// // // // // write a program to calculate the average of 3 numbers
+// // // // #include<iostream>
+// // // // using namespace std;
+// // // // int main()
+// // // // {
+// // // //     float num1, num2, num3, average;
+// // // //     cout<<"enter num1 num2 and num3";
+// // // //     cin>>num1>>num2>>num3;
+// // // //     average = (num1+num2+num3)/3;
+// // // //     cout<<"average of 3 number is:"<<average;
+// // // //     return 0;
+// // // // }
+// // // // write a c++ program to calculate the square of a number
+// // // #include<iostream>
+// // // using namespace std;
+// // // int main()
+// // // {
+// // //     int number, square;
+// // //     cout<<"enter a number:";
+// // //     cin>>number;
+// // //     square = number*number;
+// // //     cout<<"square of "<<number<<" is: "<<square;
+// // //     return 0;
+// // // }
+
+// // /*write a c++ program to calculte the value of two int variable without 
+// // using thid variable*/
+// // #include<iostream>
+// // using namespace std;
+// // int main()
+// // {
+// //     int a , b;
+// //     cout<<"enter two numbers:";
+// //     cin>>a>>b;
+// //     a = a + b;
+// //     b = a - b;
+// //     a = a - b;
+// //     cout<<"a = "<<a<<endl<<"b = "<<b;
+// //     return 0;
+// // }
+
+// /*write a c++ program to find the maximum of two numbers*/
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int a , b, maximum;
+//     cout<<"enter two number:";
+//     cin>>a>>b;
+//     maximum = a>b?a:b;
+//     cout<<"maximum of two number is:"<<maximum;
+//     return 0;
+// }
+
+
+//write a c++ program to add all the numbers of an array of size 10
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[10] = {1,2,1,2,1,1,1,1,1,10}, i , sum = 0;
+    for(i = 0; i < 10; i++){
+        sum += arr[i];
+    }
+    cout<<"add: "<<sum;
+    return 0;
+}
